76322b7567d2bf8e63d43e6a17538d59fba6010a
X-Hades-X <maurus.daehler@protonmail.com>
2022-11-04 07:41:13 +0100

    add theme, crop images, add result bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,6 @@ export class AppComponent {
 
   constructor(private iconReg: SvgIconRegistryService) {
     this.iconReg.loadSvg('../../../assets/icons/bot.svg', 'bot')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('../../../assets/icons/arrow.svg', 'arrow')?.pipe(take(1)).subscribe();
   }
 }
