c042a9a13f4578eb935809d3fdc560bcc4eb55fb
X-Hades-X <maurus.daehler@protonmail.com>
2022-11-22 16:22:15 +0100

    fix test

--- src/app/components/information/information.component.spec.ts
+++ src/app/components/information/information.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { InformationComponent } from './information.component';
 
@@ -8,9 +13,23 @@ describe('InformationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InformationComponent ]
-    })
-    .compileComponents();
+      declarations: [InformationComponent],
+      imports: [
+        MatButtonModule,
+        MatDialogModule,
+        MatIconModule,
+        AngularSvgIconModule.forRoot(),
+        HttpClientTestingModule,
+      ],
+      providers: [
+        {
+          // I was expecting this will pass the desired value
+          provide: MAT_DIALOG_DATA,
+          useValue: {},
+        },
+        { provide: MatDialogRef, useValue: {} },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InformationComponent);
     component = fixture.componentInstance;
