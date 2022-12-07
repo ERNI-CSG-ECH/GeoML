748827812a72a8b5b778f012f0c4189cfd6e4b8d
X-Hades-X <maurus.daehler@protonmail.com>
2022-12-07 16:29:25 +0100

    fix tests

--- src/app/components/explanation/explanation.component.spec.ts
+++ src/app/components/explanation/explanation.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { ExplanationComponent } from './explanation.component';
 
@@ -8,9 +13,16 @@ describe('ExplanationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExplanationComponent ]
-    })
-    .compileComponents();
+      declarations: [ExplanationComponent],
+      imports: [
+        MatButtonModule,
+        MatDialogModule,
+        MatIconModule,
+        AngularSvgIconModule.forRoot(),
+        HttpClientTestingModule,
+      ],
+      providers: [{ provide: MatDialogRef, useValue: {} }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ExplanationComponent);
     component = fixture.componentInstance;
--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,7 +13,14 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [
+        MatButtonModule,
+        MatDialogModule,
+        MatIconModule,
+        AngularSvgIconModule.forRoot(),
+        HttpClientTestingModule,
+      ],
     })
     .compileComponents();
 
