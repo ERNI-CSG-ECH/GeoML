d40c839012f19ebff04ca1d2a611ee91a86f51a0
X-Hades-X <maurus.daehler@protonmail.com>
2023-02-13 14:50:10 +0100

    fix test and deploy after build

--- src/app/components/estimation/estimation.component.spec.ts
+++ src/app/components/estimation/estimation.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MATERIAL_MODULES } from 'src/app/app.module';
 
 import { EstimationComponent } from './estimation.component';
 import { GameService } from 'src/app/services/game.service';
+import { of } from 'rxjs';
 
 describe('EstimationComponent', () => {
   let component: EstimationComponent;
   let fixture: ComponentFixture<EstimationComponent>;
 
   beforeEach(async () => {
-    const gameMock = { randomTasks$: new Promise(() => []) };
+    const gameMock = { randomTasks$: of([]) };
     await TestBed.configureTestingModule({
       declarations: [EstimationComponent],
       imports: [...MATERIAL_MODULES],
