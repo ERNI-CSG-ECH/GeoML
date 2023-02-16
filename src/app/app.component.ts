1d706da23814f0abca48c4cb414db5dc70495c20
X-Hades-X <maurus.daehler@protonmail.com>
2023-02-16 16:35:25 +0100

    fix icons and move button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,19 @@ export class AppComponent {
   title = 'GeoML';
 
   constructor(private iconReg: SvgIconRegistryService) {
-    this.iconReg.loadSvg('../../../assets/icons/bot.svg', 'bot')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/bot-pin.svg', 'bot-pin')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/arrow.svg', 'arrow')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/info.svg', 'info')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/car.svg', 'car')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/ruler.svg', 'ruler')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/facebook.svg', 'facebook')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/twitter.svg', 'twitter')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/linkedin.svg', 'linkedin')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/book.svg', 'book')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/erni.svg', 'erni')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/light.svg', 'light')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/sever.svg', 'sever')?.pipe(take(1)).subscribe();
-    this.iconReg.loadSvg('../../../assets/icons/lethal.svg', 'lethal')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/bot.svg', 'bot')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/bot-pin.svg', 'bot-pin')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/arrow.svg', 'arrow')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/info.svg', 'info')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/car.svg', 'car')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/ruler.svg', 'ruler')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/facebook.svg', 'facebook')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/twitter.svg', 'twitter')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/linkedin.svg', 'linkedin')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/book.svg', 'book')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/erni.svg', 'erni')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/light.svg', 'light')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/sever.svg', 'sever')?.pipe(take(1)).subscribe();
+    this.iconReg.loadSvg('assets/icons/lethal.svg', 'lethal')?.pipe(take(1)).subscribe();
   }
 }
